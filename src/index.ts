b87e45a4464723cfcea4e834f574c693d81f12bf
shota876 <sh.guliashvili1@gmail.com>
2024-01-06 03:30:49 -0800

    Full finished TypeScript & snowpack project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,77 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { stringify, v4 as uuidV4 } from 'uuid'
 
-import confetti from 'canvas-confetti';
+type task = {id: string, title: string, completed: boolean, createAt: Date}
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const log = console.log
+
+const list = document.querySelector('#list') as HTMLUListElement
+const form = document.querySelector('#new-task-form') as HTMLFormElement
+const input = document.querySelector('#new-task-title') as HTMLInputElement
+const tasks: task[] = []
+tasks.forEach(addListItem)
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  if(input?.value === '' || input?.value === null) return
+
+  const newTask: task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createAt: new Date()
+  }
+  tasks.push(newTask)
+  saveTasks()
+
+  addListItem(newTask)
+  input.value = ''
+})
+
+function addListItem(task: task) {
+  const item = document.createElement('li')
+  item.classList.add('list-item')
+  const label = document.createElement('label')
+  const deleteBtn = document.createElement('button')
+  deleteBtn.textContent = 'Delete'
+  const checkBox = document.createElement('input')
+  checkBox.type = 'checkbox'
+
+  checkBox.addEventListener('change', () => {
+    task.completed = checkBox.checked
+    if(task.completed === true){
+      item.classList.add('task-completed')
+    }else{
+      item.classList.remove('task-completed')
+    }    
+    saveTasks()
+  })
+
+  deleteBtn.addEventListener('click', () => {
+    item.classList.add('removedItem')
+  })
+
+  checkBox.checked = task.completed
+  label.append(checkBox, task.title)
+  label.append(deleteBtn)
+  item.append(label)
+  list?.append(item)
+}
+
+function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+
+
+// function loadTasks(): task[] {
+//   const taskJSON = localStorage.getItem('tasks')
+//   if(taskJSON === null) return []
+//   return JSON.parse(taskJSON)
+// }
+
+
+
+
+
+
+
